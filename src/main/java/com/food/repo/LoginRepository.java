db2aeb8c5953a873383815b46d3b6a6b58170dc7
saksham122 <53997762+saksham122@users.noreply.github.com>
2022-10-15 17:12:37 +0530

    Delete src directory

--- src/main/java/com/food/repo/LoginRepository.java
+++
@@ -1,9 +0,0 @@
-package com.food.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.food.bean.Login;
-
-public interface LoginRepository extends JpaRepository<Login,String>{
-	
-}
